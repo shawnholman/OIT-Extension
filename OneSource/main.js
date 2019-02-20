9b0aefe7316d6ec391db04a71a47c0759d476175
Shawn Holman <shawnholman@uga.edu>
2019-02-20 10:37:42 -0500

    Defaulted Punch Type to the opposite of the last recorded punch. In->Out, Out->In

--- OneSource/main.js
+++ OneSource/main.js
@@ -4,6 +4,8 @@
     // Id's of various elements needed to manipulate
     const TIME_REPORTING_OPENER = "TL_LBL_WRK_TIME_RPTG_LBL$0"; // arrow next to "Time Reporting Elements"
     const TIME_REPORTING_INPUT = "TL_RPTD_TIME_TRC$0"; // select input next to "Time Reporting Code" under "Time Reporting Elements"
+    const PUNCH_TYPE_INPUT = "TL_RPTD_TIME_PUNCH_TYPE$0"; // select input for the "Punch Type" under "Enter Punch"
+    const LAST_RECORDED_PUNCH = "DERIVED_TL_CLK_TL_COMMENTS$246$"; // element contains information regarding the last entered punch
     const PUNCH_BUTTON = "TL_LINK_WRK_TL_SAVE_PB$0"; // punch button element
     const SIGNOUT = "pthdr2signout"; // signout button in the dropdown menue
 
@@ -22,12 +24,25 @@
         }, 10);
         return int;
     }
+    
     let iframe = document.getElementById("ptifrmtgtframe");
 
     // wait for the iFrame before we can mess with it
     iframe.onload = function () {
         let frameWindow = iframe.contentWindow;
         let doc = frameWindow.document;
+        
+        // just to break things up little, this function takes care of setting the punch type and time reporting code inputs
+        function setInputs () {
+            // the innerText of the last recording punch format "{punchtype} at hh:mm:ss"..thats why splitting at getting the first element gives up the last punch type
+            let lastPunchType = doc.getElementById(LAST_RECORDED_PUNCH).innerText.split(" ")[0].toLowerCase(); 
+            let defaultPunchType = lastPunchType == "in" ? 2 : 1; // "2" => "out" and "1" => "in" .. so if we were last in we should default punch type to out and vice verse
+
+            doc.getElementById(PUNCH_TYPE_INPUT).value = defaultPunchType;
+            // set the time reporting code to regular
+            doc.getElementById(TIME_REPORTING_INPUT).value = "00REG";
+        }
+
 
         // open the time reporting element
         doc.getElementById(TIME_REPORTING_OPENER).click();
@@ -53,8 +68,7 @@
                 });
             };
 
-            // set the time reporting code to regular
-            doc.getElementById(TIME_REPORTING_INPUT).value = "00REG";
+            setInputs();
         });
     };
 })();
