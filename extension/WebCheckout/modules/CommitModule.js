1e9f3412460056e0a5ca29165357a31553abb357
Media Desk <mediadsk@uga.edu>
2021-05-13 14:08:06 -0400

    Updated the CommitModule to work with Webcheckout 9.0

--- extension/WebCheckout/modules/CommitModule.js
+++ extension/WebCheckout/modules/CommitModule.js
@@ -2,44 +2,44 @@ import {Requests} from '../requests.js';
 import {Utility} from '../util.js';
 
 export class CommitModule {
-    async _showErrorBox () {
-        let errorList = "";
-        $('.messages .message-error').each(function () {
-            errorList += "<li>" + $(this).text() + "</li>";
-        });
+    async _showErrorBox (message) {
+        let errorList = `
+            <li>${message}</li>
+        `;
         let content = await Utility.pullResource('WebCheckout/templates/commit_confirm/commit-error.html', {errors: errorList});
         Utility.openLightBox(content, function () {}); 
     }
     
     async _checkout(allocationId) {
-        if ($('.messages .message-error').length > 0) {
-            this._showErrorBox();
+        if ($(".item-unavailabe-icon").length > 0) {
+            this._showErrorBox("One or more resources are not available.");
+            return;
+        }
+
+        if ($('.selectedResourceItem').length == 0) {
+            this._showErrorBox("No resources or resource types selected.");
             return;
         }
         
         let content = await Utility.pullResource('WebCheckout/templates/commit_confirm/index.html', {});
         Utility.openLightBox(content, function () {
+
             Requests.completeCheckout(allocationId).then(function () {
                 $('#commitment-message').text("Done!");
                 $('#committing-loader').toggleClass('load-complete');
                 $('#committing-loader .checkmark').toggle();
                 
                 setTimeout(function () {
-                    $(".reset-all")[0].click();
-                    $(".resources-selected-list, .patron-info").empty();
-                    $("#input-patron").val("").focus();
-                    $.featherlight.close();
-                    
+                    location.reload();
                 }, 500);
             });
         });   
     }
     
     install() {
-        $(".rp-button.submit-all")
-            .unwrap() // removes the parent "form" tag
-            .on('click', () => {
-            let allocationId = $("#allocation").text().match(/[0-9]+/)[0];
+        $("#resourceInput").parent().append(`<button id="commitNow" class="btn font-weight-bold margin-bottom-5" type="button">Checkout!</button>`);
+        $(document).on('click', '#commitNow', () => {
+            let allocationId = Utility.getAllocationId();
             this._checkout(allocationId);
         });
     }
